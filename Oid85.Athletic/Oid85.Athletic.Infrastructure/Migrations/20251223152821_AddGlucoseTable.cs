8baec5eec76c3936377e5e5be58eb5af906ee109
oid85 <oid85@mail.ru>
2025-12-23 22:30:36 +0700

    .

---
+++ Oid85.Athletic/Oid85.Athletic.Infrastructure/Migrations/20251223152821_AddGlucoseTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oid85.Athletic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGlucoseTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GlucoseEntities",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    BeforeMorningFood = table.Column<double>(type: "double precision", nullable: true),
+                    AfterMorningFood = table.Column<double>(type: "double precision", nullable: true),
+                    BeforeTraining = table.Column<double>(type: "double precision", nullable: true),
+                    AfterTraining = table.Column<double>(type: "double precision", nullable: true),
+                    BeforeEveningFood = table.Column<double>(type: "double precision", nullable: true),
+                    BeforeNight = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlucoseEntities", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GlucoseEntities",
+                schema: "public");
+        }
+    }
+}
