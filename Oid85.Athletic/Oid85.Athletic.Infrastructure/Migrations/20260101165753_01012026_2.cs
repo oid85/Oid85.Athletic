ceb67046bc5f9af4b09a3ad503e6586b0023945b
oid85 <oid85@mail.ru>
2026-01-01 23:58:55 +0700

    .

---
+++ Oid85.Athletic/Oid85.Athletic.Infrastructure/Migrations/20260101165753_01012026_2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oid85.Athletic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _01012026_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlanEntities_TrainingEntities_MorningTrainingId",
+                schema: "public",
+                table: "PlanEntities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PlanEntities_MorningTrainingId",
+                schema: "public",
+                table: "PlanEntities");
+
+            migrationBuilder.DropColumn(
+                name: "MorningTrainingId",
+                schema: "public",
+                table: "PlanEntities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MorningTrainingId",
+                schema: "public",
+                table: "PlanEntities",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlanEntities_MorningTrainingId",
+                schema: "public",
+                table: "PlanEntities",
+                column: "MorningTrainingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlanEntities_TrainingEntities_MorningTrainingId",
+                schema: "public",
+                table: "PlanEntities",
+                column: "MorningTrainingId",
+                principalSchema: "public",
+                principalTable: "TrainingEntities",
+                principalColumn: "id");
+        }
+    }
+}
